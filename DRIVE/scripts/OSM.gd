9aeb880a536de30a1c444cb437bc088ab4a01557
Nathan M <82677882+hidude562@users.noreply.github.com>
2024-11-08 10:58:53 -0500

    Parsing roads and draw

--- DRIVE/scripts/OSM.gd
+++ DRIVE/scripts/OSM.gd
@@ -1,36 +1,62 @@
 extends Node3D
 
 class LatLong:
+	static var relPos = Vector2(38.6305990, -84.6324800)
 	var pos
 	
 	func _init(pos: Vector2) -> void:
 		self.pos = pos
-	
+		
+	# TODO: I have no idea how to actually calculate this lmao
+	func toFeet() -> Vector2:
+		return (self.pos - relPos) * 10000
 
-var latLong = Vector2(38.6305990, -84.6324800)
-
-class Buildable:
+class Buildable extends Node:
 	var element_name = null
 	var tags = []
-	var verts = []
+	var nodes = []
 	
-	func _init():
+	func _init() -> void:
 		pass
 	
-	func addVert(node) -> void:
-		print(node)
+	func add_node(node) -> void:
+		nodes.append(node)
 	
 	func construct() -> void:
 		pass
 
 class Way extends Buildable:
-	func _init():
-		element_name = "Way"
+	func _init() -> void:
+		element_name = "way"
 	
 	func construct() -> void:
+		var path = Path3D.new()
+		path.curve = Curve3D.new()
 		# Build a path
-		for i in verts:
-			pass
+		print("Constructing...")
+		for i in nodes:
+			# Construct coordinates
+			var coords = LatLong.new(
+				Vector2(
+					float(i['lat']),
+					float(i['lon'])
+				)
+			)
+			var coordsFeet = coords.toFeet()
+			var coordsAndElevation = Vector3(coordsFeet.x, 0-2, coordsFeet.y)
+			path.curve.add_point(coordsAndElevation)
+		
+		
+		get_node("./").add_child(path)
+		
+		# CSG polygon
+		var csg = CSGPolygon3D.new()
+		csg.mode = CSGPolygon3D.MODE_PATH
+		print(path.get_path())
+		csg.path_node = path.get_path()
+		
+		# Add csg to path
+		path.add_child(csg)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -38,7 +64,7 @@ func _ready():
 	var nodes = {}
 	var current_element = ""
 	var encapsulation_type = null
-	parser.open("assets/map.osm")
+	parser.open("assets/NYC.osm")
 	
 	while parser.read() != ERR_FILE_EOF:
 		var node_type = parser.get_node_type()
@@ -49,15 +75,19 @@ func _ready():
 			var attributes_dict = {}
 			for idx in range(parser.get_attribute_count()):
 				attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
-			nodes.merge({attributes_dict.get("id"): attributes_dict})
+			nodes.merge({attributes_dict.get('id'): attributes_dict})
 		elif node_type == XMLParser.NODE_TEXT:
 			var attributes_dict = {}
 			for idx in range(parser.get_attribute_count()):
 				attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
 			if current_element == 'way':
 				encapsulation_type = Way.new()
+			elif current_element == 'nd':
+				if encapsulation_type != null:
+					encapsulation_type.add_node(nodes[attributes_dict['ref']])
 			
 		elif node_type == XMLParser.NODE_ELEMENT_END:
 			if encapsulation_type != null and encapsulation_type.element_name == node_name:
+				add_child(encapsulation_type)
 				encapsulation_type.construct()
 				encapsulation_type = null
