b9780b83fb2645c4929c58987ba3d49a2901baa5
Nathan M <82677882+hidude562@users.noreply.github.com>
2024-11-13 05:25:08 -0500

    Terrain generation testing

--- DRIVE/scripts/terrain.gd
+++ DRIVE/scripts/terrain.gd
@@ -13,7 +13,7 @@ func generate_terrain():
 	var lat_end = get_parent().lat2
 	var lon_start = get_parent().lon1
 	var lon_end = get_parent().lon2
-	var subdivision = 0.0005
+	var subdivision = 0.0001
 	
 	var lon = lon_start
 	while lon < lon_end:
@@ -22,7 +22,7 @@ func generate_terrain():
 		while lat < lat_end:
 			var newPoint = Globals.LatLongHeight.new(
 					Vector2(lon, lat), 
-					10
+					(cos(lon * 5000) * cos(lat * 5000) * 4) + 10
 				)
 			lat_points.append(
 				newPoint.toMeters3D()
@@ -32,24 +32,34 @@ func generate_terrain():
 		lon+=subdivision
 	
 	var arr_mesh = ArrayMesh.new()
-	#arrays.resize(Mesh.ARRAY_MAX)
-	var verts = PackedVector3Array()
-	for i in range(len(points)):
-		for j in range(len(points[i])):
-			verts.push_back(
-				points[i][j+(1 if i%3 == 0 and j!=len(points[i])-1 else 0)]
-			)
-			if len(verts) == 3:
-				
-				var arrays = []
-				arrays.resize(Mesh.ARRAY_MAX)
-				arrays[Mesh.ARRAY_VERTEX] = verts
-				print(verts)
-				arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-				
-				verts = PackedVector3Array()
+	var triangles = PackedVector3Array()
+	
+	for i in range(len(points)-1):
+		for j in range(len(points[i])-1):
+			var triangle1 = PackedVector3Array(
+					[
+						points[i][j],
+						points[i+1][j],
+						points[i][j+1]
+					]
+				)
+			var triangle2 = PackedVector3Array(
+					[
+						points[i+1][j],
+						points[i+1][j+1],
+						points[i][j+1]
+					]
+				)
+			triangles.append_array(triangle1)
+			triangles.append_array(triangle2)
+	
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = triangles
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	
 				
 	var mesh_instance = MeshInstance3D.new()
 	mesh_instance.mesh = arr_mesh
-	print("Push mesh")
+	print(len(points), " ", len(points[0]))
 	add_child(mesh_instance)
